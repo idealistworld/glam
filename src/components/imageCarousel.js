82195a3030c3e011d876754d7b5afc57c50e94f2
idealistworld <cnakayama567@gmail.com>
2023-11-24 21:51:09 -0500

    Rounded of Images

--- src/components/imageCarousel.js
+++ src/components/imageCarousel.js
@@ -8,15 +8,15 @@ function ImageCarousel() {
     <div className="flex justify-center items-center">
     <img
       src={leftImage}
-      className="w-[170px] max-lg:w-[90px] h-auto -rotate-12 rounded-3xl hover:scale-110 duration-300"
+      className="w-[170px] max-lg:w-[90px] h-auto -rotate-12 rounded-3xl hover:scale-110 duration-300 max-sm:rounded-2xl"
     ></img>
     <img
       src={middleImage}
-      className="w-[230px] max-lg:w-[150px] z-50 h-auto rounded-3xl hover:scale-110 duration-300"
+      className="w-[230px] max-lg:w-[150px] z-50 h-auto rounded-3xl hover:scale-110 duration-300 max-sm:rounded-2xl"
     ></img>
     <img
       src={rightImage}
-      className="w-[170px] max-lg:w-[90px] h-auto rotate-6 rounded-3xl hover:scale-110 duration-300"
+      className="w-[170px] max-lg:w-[90px] h-auto rotate-6 rounded-3xl hover:scale-110 duration-300 max-sm:rounded-2xl"
     ></img>
   </div>
   
