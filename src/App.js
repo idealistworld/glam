64da11d2c6eac916f819cd0d39546b29576df63a
idealistworld <cnakayama567@gmail.com>
2023-11-24 21:46:52 -0500

    Edits ! !

--- src/App.js
+++ src/App.js
@@ -21,16 +21,16 @@ function App() {
         </div>
       </header>
       <body id='product' class='py-14 flex-col items-center justify-center'>
-        <div class='flex flex-wrap-reverse px-7 py-20 max-sm:py-10 justify-center items-center bg-landing bg-cover max-lg:text-center'>
+        <div class='flex flex-wrap-reverse px-7 py-20 max-sm:py-11 justify-center items-center bg-landing bg-cover max-lg:text-center'>
           <div class='max-lg:w-[100%] max-lg:mt-14 w-[40%] mr-[80px] max-lg:mr-[0px] z-30'>
             <h1 class='max-sm:text-left max-lg:text-4xl text-white font-black text-6xl drop-shadow-2xl max-lg:leading-tight leading-tight'>✨ make custom <span class='animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>ai generated </span> filters ✨</h1>
             <p class='max-sm:text-left max-sm:text-base max-lg:text-md text-white font-medium text-lg leading-loose mt-3'>Making your content eye-catching and the one that stands out among the others’ content?  Check Glam app’s AI Magic that is a perfect tool for your photos and videos!</p>
             <div class='flex mt-12 max-lg:justify-center'>
               <a href='https://apps.apple.com/sa/app/glam-ai-art-video-generator/id1545593132' target='_blank' className='mb-4'>
-                <img src={appStore} className='transform drop-shadow-2xl max-md:h-12 h-14 hover:scale-110 duration-300 hover:-rotate-6'></img>
+                <img src={appStore} className='transform drop-shadow-2xl max-md:h-11 h-14 hover:scale-110 duration-300 hover:-rotate-6'></img>
               </a>
               <a href='https://play.google.com/store/apps/details?id=app.getglam&hl=en_US&gl=US' target='_blank'>
-                <img src={googlePlay} className='ml-10 transform drop-shadow-2xl max-md:h-12 h-14 hover:scale-110 duration-300 hover:rotate-6'></img>
+                <img src={googlePlay} className='ml-10 transform drop-shadow-2xl max-md:h-11 h-14 hover:scale-110 duration-300 hover:rotate-6'></img>
               </a>
             </div>
           </div>
@@ -39,7 +39,7 @@ function App() {
           </div>
         </div>
 
-        <div class='py-10 px-5 my-8 bg-white flex flex-wrap items-center justify-around max-md:justify-start max-lg:py-0'>
+        <div class='py-10 max-sm:px-8 px-5 my-8 bg-white flex flex-wrap items-center justify-around max-md:justify-start max-lg:py-0 max-lg:mb-3'>
           <h1 class='text-3xl font-bold w-32 text-neutral-500 mr-10 max-sm:mx-0'>In <span class='text-4xl text-black whitespace-nowrap'> just one </span> year</h1>
           <div class='flex flex-col flex-row items-start max-sm:mx-0 max-sm:w-screen max-sm:mt-10'>
             <h1 class='text-6xl max-sm:text-7xl max-lg:text-5xl font-bold animate-text bg-gradient-to-r from-[#FF8FFA] via-[#FFCDB1] to-[#FFEB69] bg-clip-text text-transparent font-white'>500K+</h1>
