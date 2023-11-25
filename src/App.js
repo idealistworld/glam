4930607e97580c3c638655e657cdf72198d948d6
idealistworld <cnakayama567@gmail.com>
2023-11-24 20:06:36 -0500

    Landing spacing

--- src/App.js
+++ src/App.js
@@ -21,16 +21,16 @@ function App() {
         </div>
       </header>
       <body id='product' class ='py-14 flex-col items-center justify-center '>
-        <div class='flex flex-wrap-reverse px-7 py-20 justify-center items-center bg-landing bg-cover'>
+        <div class='flex flex-wrap-reverse px-7 py-20 justify-center items-center bg-landing bg-cover max-lg:text-center'>
           <div class ='max-lg:w-[100%] max-lg:mt-14 w-[40%] mr-[80px] max-lg:mr-[0px] z-30'>
-            <h1 class='max-lg:text-6xl text-white font-black text-6xl drop-shadow-2xl max-lg:leading-tight leading-tight'>✨ make custom <span class ='animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>ai generated </span> filters ✨</h1>
-            <p class ='text-white font-medium text-lg leading-loose mt-3'>Making your content eye-catching and the one that stands out among the others’ content?  Check Glam app’s AI Magic that is a perfect tool for your photos and videos!</p>
-            <div class = 'max-sm:flex-col flex mt-12'>
+            <h1 class='max-sm:text-left max-lg:text-5xl text-white font-black text-6xl drop-shadow-2xl max-lg:leading-tight leading-tight'>✨ make custom <span class ='animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>ai generated </span> filters ✨</h1>
+            <p class ='max-sm:text-left max-lg:text-md text-white font-medium text-lg leading-loose mt-3'>Making your content eye-catching and the one that stands out among the others’ content?  Check Glam app’s AI Magic that is a perfect tool for your photos and videos!</p>
+            <div class = 'flex mt-12 max-lg:justify-center'>
               <a href='https://apps.apple.com/sa/app/glam-ai-art-video-generator/id1545593132' target='_blank'>
               <img src={appStore} class ='transform drop-shadow-2xl max-lg:h-14 h-14 hover:scale-110 duration-300 hover:-rotate-6'></img> 
               </a>
               <a href = 'https://play.google.com/store/apps/details?id=app.getglam&hl=en_US&gl=US' target='_blank'>    
-              <img src={googlePlay} class ='max-sm:ml-0 max-sm:mt-10 max-lg:h-14 ml-6 transform drop-shadow-2xl h-14 hover:scale-110 duration-300 hover:rotate-6'></img>          
+              <img src={googlePlay} class ='max-sm:ml-10 max-sm:mt-0 max-lg:h-14 ml-6 transform drop-shadow-2xl h-14 hover:scale-110 duration-300 hover:rotate-6'></img>          
               </a>
             </div>
           </div>
@@ -39,7 +39,7 @@ function App() {
           </div>
         </div>
         
-        <div class='py-10 px-5 my-8 bg-white flex flex-wrap items-center justify-around max-md:py-7 max-lg:justify-start'>
+        <div class='py-10 px-5 my-8 bg-white flex flex-wrap items-center justify-around max-lg:py-4 max-lg:justify-start'>
           <h1 class='text-3xl font-black w-32 text-neutral-500 mr-10 mx-5 max-md:mx-0'>In <span class = 'text-4xl text-black whitespace-nowrap'> just one </span> year</h1>
           <div class ='flex flex-col flex-row items-start mx-5 max-md:mx-0 max-md:w-screen max-lg:mt-10'>
             <h1 class='text-6xl max-lg:text-7xl font-bold animate-text bg-gradient-to-r from-[#FF8FFA] via-[#FFCDB1] to-[#FFEB69] bg-clip-text text-transparent font-white'>500K+</h1>
