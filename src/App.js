268c9d641955dc62d367fdb25c494e59ef4da9fa
idealistworld <cnakayama567@gmail.com>
2024-01-15 12:23:33 -0800

    didn't work

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <body id='product' class='flex-col items-center justify-center overflow-hidden'>
         <div class='flex flex-wrap-reverse px-7 pt-28 pb-10 justify-center items-center bg-landing bg-cover max-lg:text-center'>
           <div class='max-lg:w-[100%] max-lg:mt-10 w-[40%] mr-[80px] max-lg:mr-[0px] z-30'>
-            <h1 class='max-sm:text-left max-lg:text-4xl text-white font-black text-6xl drop-shadow-2xl max-lg:leading-tight leading-tight'>✨ Make custom <span class='animate-text-test bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>ai generated </span> filters ✨</h1>
+            <h1 class='max-sm:text-left max-lg:text-4xl text-white font-black text-6xl drop-shadow-2xl max-lg:leading-tight leading-tight'>✨ Make custom <span class='animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>ai generated </span> filters ✨</h1>
             <p class='max-sm:text-left max-sm:text-base max-lg:text-md text-white font-medium text-lg leading-loose mt-3'>Making your content eye-catching and the one that stands out among the others’ content?  Check Glam app’s AI Magic that is a perfect tool for your photos and videos!</p>
             <div class='flex mt-12 max-lg:justify-center max-sm:mt-10'>
               <a href='https://apps.apple.com/sa/app/glam-ai-art-video-generator/id1545593132' target='_blank' className='mb-4'>
@@ -41,15 +41,15 @@ function App() {
         <div class='py-4 max-sm:px-10 px-5 my-8 bg-white flex flex-wrap items-center justify-around max-md:justify-start max-lg:py-0 max-lg:mb-3'>
           <h1 class='text-3xl font-bold w-32 text-neutral-500 mr-10 max-sm:mx-0'>In <span class='text-4xl text-black whitespace-nowrap'> just one </span> year</h1>
           <div class='flex flex-col flex-row items-start max-md:mx-2 max-sm:mx-0 max-sm:w-screen max-sm:mt-10'>
-            <h1 class='text-6xl max-sm:text-7xl max-lg:text-5xl font-bold animate-text-test-test bg-gradient-to-r from-[#FF8FFA] via-[#FFCDB1] to-[#FFEB69] bg-clip-text text-transparent font-white'>500K+</h1>
+            <h1 class='text-6xl max-sm:text-7xl max-lg:text-5xl font-bold animate-text bg-gradient-to-r from-[#FF8FFA] via-[#FFCDB1] to-[#FFEB69] bg-clip-text text-transparent font-white'>500K+</h1>
             <h1 class='text-2xl font-bold mt-3 text-neutral-500 tracking-wide'>downloads</h1>
           </div>
           <div class='flex flex-col flex-row items-start max-md:mx-2 max-sm:mx-0 max-sm:w-screen max-sm:mt-10'>
-            <h1 class='text-6xl max-sm:text-7xl max-lg:text-5xl font-bold animate-text-test-test bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>$155k</h1>
+            <h1 class='text-6xl max-sm:text-7xl max-lg:text-5xl font-bold animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>$155k</h1>
             <h1 class='text-2xl font-bold mt-3 text-neutral-500 tracking-wide'>MRR</h1>
           </div>
           <div class='flex flex-col flex-row items-start max-md:mx-2 max-sm:mx-0 max-sm:w-screen max-sm:mt-10'>
-            <h1 class='text-6xl max-sm:text-7xl max-lg:text-5xl font-bold animate-text-test-test bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>top 10</h1>
+            <h1 class='text-6xl max-sm:text-7xl max-lg:text-5xl font-bold animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>top 10</h1>
             <h1 class='text-2xl font-bold mt-3 text-neutral-500 tracking-wide'>ranking</h1>
           </div>
         </div>
