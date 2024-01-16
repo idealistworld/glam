50a3f4b49b506e2eeec018387db60544831c20a4
idealistworld <cnakayama567@gmail.com>
2024-01-15 21:19:37 -0800

    lol

--- src/pages/howitworks.js
+++ src/pages/howitworks.js
@@ -1,12 +1,29 @@
 import '../components/HowItWorks.css';
 
+import { useEffect } from 'react';
 
-export default function asdf() {
+export default function Asdf() {
+
+    useEffect(() => {
+        // Create a script element
+        const script = document.createElement('script');
+        script.src = "//www.instagram.com/embed.js";
+        script.async = true;
+
+        // Append the script to the body
+        document.body.appendChild(script);
+
+        // Cleanup function to remove script when component unmounts
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (<div class='bg-cover max-lg:py-16 py-28 min-h-[100vh] bg-landing flex flex-col items-center'>
         <div className='flex-wrap relative bg-cover flex items-center justify-around w-[85%]'>
 
             <div className='box-border max-md:w-[100%] max-lg:px-5 max-lg:pb-0 py-12 px-4 rounded-3xl w-[100%] max-md:mb-0 flex-wrap'>
-                <h1 className='max-lg:text-6xl max-lg:mt-0 text-7xl font-black bg-clip-text text-white '>How <span class='animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>Glam App</span> works</h1>
+                <h1 className='max-lg:text-6xl max-lg:mt-0 text-7xl font-black bg-clip-text text-white '>Glam <span class='animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'> how it works</span></h1>
                 <p className='max-md:text-sm max-lg:mb-10 mt-7 text-xl text-white leading-10 opacity-75'>Glam is super easy to get started with. All you need is the Glam app and content and in a few minutes, you'll have finished content. Our goal is to democratize content creation and allow anyone to have access to the tools that professional influencers use.</p>
             </div>
 
@@ -32,9 +49,8 @@ export default function asdf() {
                     <h1 className='max-lg:text-3xl max-lg:mt-0 text-3xl font-black bg-clip-text animate-text text-white'>4. Share the result</h1>
                     <p className='max-lg:text-sm mt-5 text-md text-white leading-8 opacity-75'>Enjoy your new content! Post your AI-powered photo or video on platforms such as TikTok or Instagram!</p>
                 </div>
-
             </div>
-
         </div>
     </div>)
 }
+
