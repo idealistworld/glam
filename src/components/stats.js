48ba536cc14625d6e554f04a410eb2bfc952d5bd
idealistworld <cnakayama567@gmail.com>
2024-01-17 00:57:00 -0800

    lol

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,9 +1,9 @@
 export default function Stats() {
-    return (<div class='w-[100%] gap-[30px] p-12 max-sm:px-10 my-4 backdrop-blur bg-white/10 border-[1px] px-12 border-white rounded-xl flex flex-wrap justify-between max-md:justify-start'>
+    return (<div class='w-[100%] gap-[30px] py-8 max-sm:px-10 mt-12 backdrop-blur px-12 border-white rounded-xl flex flex-wrap justify-between max-md:justify-start px-4'>
         <h1 class='text-3xl font-bold text-white max-sm:mx-0'>In just one year, <br></br> we've accomplished:</h1>
         <div class='flex flex-col flex-row items-start max-md:mx-2 max-sm:mx-0 max-sm:w-screen max-sm:mt-4'>
             <h1 class='text-6xl max-sm:text-6xl max-lg:text-5xl font-bold animate-text bg-gradient-to-r from-[#FF8FFA] via-[#FFCDB1] to-[#FFEB69] bg-clip-text text-transparent font-white'>500K+</h1>
-            <h1 class='text-2xl font-bold mt-3 text-white tracking-wide opacity-75'>downloads</h1>
+            <h1 class='text-2xl font-bold mt-3 text-white tracking-wide'>downloads</h1>
         </div>
         <div class='flex flex-col flex-row items-start max-md:mx-2 max-sm:mx-0 max-sm:w-screen max-sm:mt-4'>
             <h1 class='text-6xl max-sm:text-6xl max-lg:text-5xl font-bold animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>$250k</h1>
@@ -11,7 +11,7 @@ export default function Stats() {
         </div>
         <div class='flex flex-col flex-row items-start max-md:mx-2 max-sm:mx-0 max-sm:w-screen max-sm:mt-4'>
             <h1 class='text-6xl max-sm:text-6xl max-lg:text-5xl font-bold animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>Top 10</h1>
-            <h1 class='text-2xl font-bold mt-3 text-white tracking-wide'>ranking</h1>
+            <h1 class='text-2xl font-bold mt-3 text-white tracking-wide'>iOS ranking</h1>
         </div>
     </div>);
 }
--- src/pages/enterprise.js
+++ src/pages/enterprise.js
@@ -4,11 +4,27 @@ export default function Enterprise() {
     return (<div>
         <div class='bg-cover max-lg:py-16 py-28 min-h-[100vh] bg-landing flex flex-col items-center'>
             <div className='flex-wrap relative bg-cover flex items-center justify-around w-[90%]'>
+
                 <div className='box-border max-md:w-[100%] max-lg:px-5 max-lg:pb-0 py-12 px-4 rounded-3xl w-[100%] max-md:mb-0 flex-wrap'>
+
                     <h1 className='max-lg:text-6xl max-lg:mt-0 text-7xl font-black bg-clip-text text-white '>Work with Glam</h1>
                     <p className='max-md:text-sm max-lg:mb-10 mt-7 text-xl text-white leading-10 opacity-75'>Our team at Glam is pushing the boundaries as to what is possible in the world of artifical intelligence.  If you'd like to partner with us, we're open to it!  No deal is too big or too small, so feel free to reach out.</p>
                 </div>
+                <div class='px-4 flex gap-[30px] w-[100%] flex-wrap'>
+                    <a href='mailto:paul@neiro.ai' target='_blank'>
+                        <div class='backdrop-blur bg-white/10 border-[1px] px-12 py-2 rounded-xl hover:scale-110 duration-300'>
+                            <p className='max-lg:text-sm font-black text-white leading-8'>Contact Sales</p>
+                        </div>
+                    </a>
+
+                    <a href='https://docs.google.com/presentation/d/19A8fD4zBttLFpndWpmURhf7nyZ50pbdA/edit#slide=id.p2' target='_blank'>
+                        <div class='backdrop-blur bg-white/10 border-[1px] px-12 py-2 rounded-xl hover:scale-110 duration-300'>
+                            <p className='max-lg:text-sm font-black text-white leading-8'>View Deck</p>
+                        </div>
+                    </a>
+                </div>
                 <Stats></Stats>
+
             </div>
         </div>
     </div>)
