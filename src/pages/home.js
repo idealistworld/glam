acd7bc648cdcc0603086bc53df79cad2b621027a
idealistworld <cnakayama567@gmail.com>
2024-04-02 13:07:11 -0400

    lol

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ import HowItWorks from '../pages/howitworks'
 import Stats from '../components/stats'
 
 export default function Landing() {
-    return (<body id='product' class='flex-col items-center justify-center overflow-hidden bg-cover bg-landing'>
+    return (<body id='product' class='flex-col items-center justify-center overflow-hidden bg-cover'>
         <div class='flex flex-wrap-reverse px-7 pt-20 min-h-[100vh] justify-center items-center max-lg:text-center'>
             <div class='max-lg:w-[100%] max-lg:mt-10 w-[40%] mr-[80px] max-lg:mr-[0px] z-30'>
                 <h1 class='max-sm:text-left max-lg:text-4xl text-white font-black text-6xl drop-shadow-2xl max-lg:leading-tight leading-tight'>✨ Make custom <span class='animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>ai generated </span> filters ✨</h1>
@@ -27,9 +27,7 @@ export default function Landing() {
 
         <div id='enterprise' class='pb-28 flex flex-col items-center'>
             <div className='flex-wrap relative bg-cover flex items-center justify-around w-[90%]'>
-
                 <div className='box-border max-md:w-[100%] max-lg:px-5 max-lg:pb-0 py-12 px-4 rounded-3xl w-[100%] max-md:mb-0 flex-wrap'>
-
                     <h1 className='max-lg:text-6xl max-lg:mt-0 text-7xl font-black bg-clip-text text-white '>Let's <span class='animate-text bg-gradient-to-r from-[#FFEB69] via-[#FFA2A9] to-[#FFEB69] bg-clip-text text-transparent font-white'>collab</span></h1>
                     <p className='max-md:text-sm max-lg:mb-10 mt-7 text-xl text-white leading-10 opacity-75'>Our team at Glam is pushing the boundaries as to what is possible in the world of artifical intelligence.  If you'd like to partner with us, we're open to it!  No deal is too big or too small, so feel free to reach out.</p>
                 </div>
