aa84d65cb51c741dbe8989401cf64c26e697d697
idealistworld <cnakayama567@gmail.com>
2024-04-02 12:59:36 -0400

    lol

--- src/components/header.js
+++ src/components/header.js
@@ -25,10 +25,10 @@ export default function Header() {
             <div className='flex mr-6 max-sm:mr-3'>
                 {/* Updated Link components to use onClick for scrolling */}
                 {/* Use <a> instead of <Link> if you're only scrolling on the same page */}
-                <a href='/#howitworks' onClick={handleScroll('howitworks')} className='text-xl mr-[30px] max-lg:mr-[15px] text-[#FFFFFF] font-medium hover:scale-110 duration-200 cursor-pointer'>
+                <a href='/#howitworks' onClick={handleScroll('howitworks')} className='text-md mr-[30px] max-lg:mr-[15px] text-[#FFFFFF] font-medium hover:scale-110 duration-200 cursor-pointer'>
                     How it Works
                 </a>
-                <a href='/#enterprise' onClick={handleScroll('enterprise')} className='text-xl text-[#FFFFFF] font-medium hover:scale-110 duration-200 cursor-pointer'>
+                <a href='/#enterprise' onClick={handleScroll('enterprise')} className='text-md text-[#FFFFFF] font-medium hover:scale-110 duration-200 cursor-pointer'>
                     Enterprise
                 </a>
             </div>
